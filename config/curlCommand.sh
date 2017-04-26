47d29ffd5048134bdcfe43d16d45e25491c29b3a
Raj Mehra <rajmehra@au1.ibm.com>
2017-04-26 11:14:41 +1000

    Update URL

--- config/curlCommand.sh
+++ config/curlCommand.sh
@@ -1,17 +1,17 @@
-curl -vX POST http://localhost:3000/api/generateData -d @specAddress.json \
+curl -vX POST http://rm1-datagen.mybluemix.net/api/generateData -d @specAddress.json \
 --header "Content-Type: application/json" -o ./Address.csv
 
-curl -vX POST http://localhost:3000/api/generateData -d @specUser.json \
+curl -vX POST http://rm1-datagen.mybluemix.net/api/generateData -d @specUser.json \
 --header "Content-Type: application/json" -o ./User.csv
 
-curl -vX POST http://localhost:3000/api/generateData -d @specPartner.json \
+curl -vX POST http://rm1-datagen.mybluemix.net/api/generateData -d @specPartner.json \
 --header "Content-Type: application/json" -o ./Partner.sql
 
-curl -vX POST http://localhost:3000/api/generateData -d @specPortalUser.json \
+curl -vX POST http://rm1-datagen.mybluemix.net/api/generateData -d @specPortalUser.json \
 --header "Content-Type: application/json" -o ./PortalUser.sql
 
-curl -vX POST http://localhost:3000/api/generateData -d @specBillingEntity.json \
+curl -vX POST http://rm1-datagen.mybluemix.net/api/generateData -d @specBillingEntity.json \
 --header "Content-Type: application/json" -o ./BillingEntity.sql
 
-curl -vX POST http://localhost:3000/api/generateData -d @specSubscription.json \
+curl -vX POST http://rm1-datagen.mybluemix.net/api/generateData -d @specSubscription.json \
 --header "Content-Type: application/json" -o ./Subscription.sql
